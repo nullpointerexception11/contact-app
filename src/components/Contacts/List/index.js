9a8637bb883eef8f234f1fa487105cac0117d7a3
lostcode <lostcoding11@gmail.com>
2022-05-10 12:00:54 +0300

    list component updated

--- src/components/Contacts/List/index.js
+++ src/components/Contacts/List/index.js
@@ -19,7 +19,10 @@ function List({ contacts }) {
       />
       <ul className="list">
         {filtered.map((contact, i) => (
-          <li key={i}>{contact.fullname}</li>
+          <li key={i}>
+           <span>{contact.fullname}</span> 
+           <span>{contact.phone_number}</span>
+           </li>
         ))}
       </ul>
       <p>Total contacts ({filtered.length})</p>
